21094f9b049b6fbd1a9e0d158b5f305057d43fe1
John Davis <john.davis.m@protonmail.com>
2017-10-25 00:58:46 -0600

    Added Ransomware Prevention File for Bad Rabbit

---
+++ Ransomware Prevention/Badrabbit Prevention.bat
@@ -0,0 +1,9 @@
+REM Prevents Bad Rabbit Ransomware
+REM Vaccine by Amit Serper @0xAmit
+REM I added a line that removes inheritance from the command line instead of editing via the GUI
+
+REM Creates infpub.dat and cscc.dat in the C:\Windows directory
+echo > C:\Windows\infpub.dat && echo > C:\Windows\cscc.dat
+
+REM Removes inheritance from previously created files
+icacls C:\Windows\infpub.dat /inheritance:d && icacls C:\Windows\cscc.dat /inheritance:d
