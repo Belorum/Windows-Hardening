68cf721ca5d1f8135c223a6611722db70d549289
John Davis <john.davis.m@protonmail.com>
2017-10-29 22:36:47 -0600

    Uses Adminstrator SID for XML File

--- Scripts/SetNetworkToPublic/SetNetworkToPublic.bat
+++ Scripts/SetNetworkToPublic/SetNetworkToPublic.bat
@@ -1,4 +1,22 @@
 @echo off
 
+REM Inputs first part of XML file for scheduled task
+type "%~dp0P1" > SetsAllNetworksToPublic.xml
+
+REM Gets Administrators SID to input into XML file
+for /f "delims= " %%a in ('"wmic path win32_useraccount where name='Administrator' get sid"') do (
+   if not "%%a"=="SID" (          
+      set SID=%%a
+	  goto loop
+      )   
+)
+:loop
+
+echo. >> SetsAllNetworksToPublic.xml
+echo       ^<UserId^>%SID%^</UserId^> >> SetsAllNetworksToPublic.xml
+
+REM Inputs second part of XML file for scheduled task
+type "%~dp0P2" >> SetsAllNetworksToPublic.xml
+
 REM Imports an XML file that will run a powershell command to set all networks to public
-schtasks /create /xml "%~dp0SetNetworksToPublic.xml" /tn "Sets All Networks to Public"
+schtasks /create /xml "%~dp0SetsAllNetworksToPublic.xml" /tn "Sets All Networks to Public"
